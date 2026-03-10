142b740649e1a95d06b4b64de361731dab52c8e2
Garvey Fils-Aimé <300155927@monboreal.ca>
2026-03-10 17:39:41 -0400

    Ajout de la methode afficher_infos

--- voiture.py
+++ voiture.py
@@ -6,3 +6,9 @@ class Voiture :
         self.kilometrage = kilometrage
         self.chauffeur=None
 
+    def afficher_infos(self):
+        print(f"Matricule: {self.matricule},Annee: {self.annee},Marque: {self.marque},kilometrage:{self.kilometrage}")
+        if self.chauffeur==None:
+            print("Aucun chauffeur ")
+        else:
+            print(f"chauffeur: {self.chauffeur.nom},{self.chauffeur.prenom} ")
