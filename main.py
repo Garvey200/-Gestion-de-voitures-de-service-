6a9620f2b8d6db4486d2f539e0f7794073939c7a
Garvey Fils-Aimé <300155927@monboreal.ca>
2026-03-10 18:15:10 -0400

    Creation classe principale

---
+++ main.py
@@ -0,0 +1,10 @@
+from voiture import Voiture
+from employe import Employe
+
+e1=Employe("p125","Jhon","Dagrun")
+e2=Employe("p129","Nadia","Shafo")
+
+v1=Voiture("AA123","2025","Toyota",50000)
+v2=Voiture("BB689","2026","BMW",56)
+
+
