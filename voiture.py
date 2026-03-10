bc6bcd1f788b76e85abeeb8b7b0bcc7f38dfb0a3
Garvey Fils-Aimé <300155927@monboreal.ca>
2026-03-10 17:27:41 -0400

    Creation classe voiture

---
+++ voiture.py
@@ -0,0 +1,8 @@
+class Voiture :
+    def __init__(self,matricule,annee,marque,kilometrage):
+        self.matricule = matricule
+        self.annee = annee
+        self.marque = marque
+        self.kilometrage = kilometrage
+        self.chauffeur=None
+
